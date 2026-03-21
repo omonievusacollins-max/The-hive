1bfce8620d030e16fb2482f08d240f26717f0a24
omonievusacolline-max <omonievusacollins@gmail.com>
2026-03-21 17:09:26 -0400

    Remove admin menu render logic

--- Menu/script.js
+++ Menu/script.js
@@ -1,3 +1,9 @@
+
+
+
+
+
+
 fetch('menu.json')
 .then(response => response.json())
 .then(menu => {
@@ -21,26 +27,4 @@ fetch('menu.json')
         `
         // grid.innerHTML += card;
     })
-
-if (adminGrid) {
-  Object.entries(menu).forEach(([category, items]) => {
-    let adminCard = `
-      <div class="card">
-        <h3>${category}</h3>
-        <button>Edit</button>
-        <ul>
-          ${items.map(item => `
-            <li>
-              <span>${item.name}</span>
-              <span>₦${item.price.toLocaleString()}</span>
-            </li>
-          `).join("")}
-        </ul>
-      </div>
-    `;
-    adminGrid.innerHTML += adminCard;
-  });
-}else{
-    console.log("Admin grid not found. Skipping admin card generation.");
-}
 })
