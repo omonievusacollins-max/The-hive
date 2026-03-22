f43e963668276bbbeff55fd161498a2002c8a3c9
omonievusacolline-max <omonievusacollins@gmail.com>
2026-03-22 16:51:15 -0400

    Export menu for other scripts to import

--- Menu/script.js
+++ Menu/script.js
@@ -2,36 +2,7 @@ export async function getMenu(){
   const response = await fetch('menu.json')
   if(!response.ok){
     throw new Error('failed to fetch JSON');
-    const data = await response.json();
-    return data;
   }
+  const data = await response.json();
+  return data;
 }
-
-
-
-
-
-fetch('menu.json')
-.then(response => response.json())
-.then(menu => {
-
-    const grid = document.querySelector('.grid');
-    const adminGrid = document.querySelector('.gridAdmin')
-
-    Object.entries(menu).forEach(([category, items]) =>{
-        let card = `
-        <div class="card">
-          <h3>${category}</h3>
-          <ul>
-            ${items.map(item => `
-              <li>
-                <span>${item.name}</span>
-                <span>₦${item.price.toLocaleString()}</span>
-              </li>
-            `).join("")}
-          </ul>
-        </div>
-        `
-        // grid.innerHTML += card;
-    })
-})
