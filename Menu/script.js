f5e569eb7006f21e3f301bab726d8e199319d24c
omonievusacolline-max <omonievusacollins@gmail.com>
2026-03-22 05:01:13 -0400

    Function to import menu in other scripts

--- Menu/script.js
+++ Menu/script.js
@@ -1,4 +1,11 @@
-
+export async function getMenu(){
+  const response = await fetch('menu.json')
+  if(!response.ok){
+    throw new Error('failed to fetch JSON');
+    const data = await response.json();
+    return data;
+  }
+}
 
 
 
